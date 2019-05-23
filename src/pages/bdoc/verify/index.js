b5443fc5bf501198f670a1c4aba2c0e0fdd22af7
vasa <vasa@towardsblockchain.com>
2019-05-23 14:25:37 +0530

    feat: (SC-6) completed bdoc viewer
    License: MIT
    Signed-off-by: Vaibhav Saini <vasa@towardsblockchain.com>

--- src/pages/bdoc/verify/index.js
+++ src/pages/bdoc/verify/index.js
@@ -1,55 +1,32 @@
 import React, { Component, Fragment } from 'react'
-
-//Assets
-import back from '../../../assets/signy/images/left-arrow@2x.png'
-import upload from '../../../assets/signy/images/arrows@2x.png'
+import UploadPage from './upload'
+import LoaderPage from './loader'
+import ResultPage from './result'
 
 //Actions
-import bdocCreator from '../../../redux/bdoc/create/actions'
 import bdocReader from '../../../redux/bdoc/read/actions'
-
-//Antd
-import { Upload, message } from 'antd'
-const Dragger = Upload.Dragger
 
 import { connect } from 'react-redux'
 
 class Index extends Component {
     render() {
-        var fileList = []
-        const { filesToBdoc, readBdoc } = this.props
-        const props = {
-            /* onRemove: file => {
-                console.log("REMOVED: ", fileList)
-            }, */
-            beforeUpload: file => {
-                readBdoc(file)
-            },
-            fileList,
-        }
-        
-        return (
-        <Fragment>
-            <img src={back} />
-            <center>
-                <h1>For Verification</h1>
-                <p>
-                    Verify your Bdoc without sign-in
-                </p>
+        const { filesToBdoc, readBdoc, bdocFiles, completeReading } = this.props
+        let CurrentPage
 
-                <br/><br/>
-                <Upload {...props} style={{ cursor: 'pointer', backgroundColor: '#e4e9f0'}}>
-                    <p className="ant-upload-drag-icon">
-                        <img src={upload} />
-                    </p>
-                    <br/>
-                    <h4>Click or drag file to this area to upload</h4>
-                    <p className="ant-upload-hint">
-                        Please upload one Bdoc at a time
-                    </p>
-                </Upload>
-            </center>
-        </Fragment>
+        if(!Object.keys(bdocFiles).length) {
+            CurrentPage = UploadPage
+        }
+        else if(bdocFiles.isReading) {
+            CurrentPage = LoaderPage
+            setTimeout(() => {
+                completeReading()
+            }, 3000)
+        }
+        else {
+            CurrentPage = ResultPage
+        }
+        return (
+            <CurrentPage data={this.props}/>
         )
     }
 }
@@ -61,7 +38,8 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
     filesToBdoc: payload => dispatch(bdocCreator.filesToBdoc(payload)),
-    readBdoc: payload => dispatch(bdocReader.readBdoc(payload))
+    readBdoc: payload => dispatch(bdocReader.readBdoc(payload)),
+    completeReading: payload => dispatch(bdocReader.completeReading)
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Index)
