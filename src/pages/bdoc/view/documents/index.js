87a2d8b53145416e86e0b5b6f406ab1c34fd2e0c
pranavdaa <pranavmaheshwari4321@gmail.com>
2019-06-03 17:15:15 +0530

    Bdoc View Document changes

--- src/pages/bdoc/view/documents/index.js
+++ src/pages/bdoc/view/documents/index.js
@@ -1,32 +1,67 @@
 import React, { Component, Fragment } from 'react'
+import { Tabs } from 'antd'
+const { TabPane } = Tabs
 
 //Antd
 import { Empty } from 'antd'
 
 class Index extends Component {
-    render() {
+  render() {
+    let documents
 
-        let documents
-
-        const { pages } = this.props
-        
-        if(pages) {
-            documents = pages.map((page, index) => {
-                if(page.name.split('.')[1] === 'pdf'){
-                    return <p key={index}>
-                        <iframe src={`data:application/pdf;base64,${page.data}`} width="600" height="780" ></iframe>
-                    </p>
-                  }
-                  else{
-                    return <p key={index}>
-                        <iframe src={`data:image/jpeg;base64,${page.data}`} width="600" height="780" ></iframe>
-                    </p>
-                  }
-            })
+    const { pages } = this.props
+    if (pages) {
+      documents = pages.map((page, index) => {
+        if (page.name.split('.')[1] === 'pdf') {
+          return (
+            <TabPane
+              tab={
+                <span>
+                  <img
+                    src={
+                      'https://www.wkkf.org/-/media/images/resource-directory/pdf-placeholder.png'
+                    }
+                    width="60"
+                    height="78"
+                  />
+                </span>
+              }
+              key={index + 1}
+            >
+              <embed
+                src={`data:application/pdf;base64,${page.data}`}
+                type="application/pdf"
+                height="300px"
+                width="100%"
+              />
+            </TabPane>
+          )
+        } else {
+          return (
+            <TabPane
+              tab={
+                <span>
+                  <img src={`data:application/jpeg;base64,${page.data}`} width="60" height="78" />
+                </span>
+              }
+              key={index + 1}
+            >
+              <img src={`data:image/jpeg;base64,${page.data}`} width="60" height="78" />
+            </TabPane>
+          )
         }
-
-        return <Fragment>{documents || <Empty />}</Fragment>
+      })
     }
+
+    return (
+      <Fragment>
+        {' '}
+        <Tabs defaultActiveKey="1" tabPosition={'right'}>
+          {documents || <Empty />}
+        </Tabs>
+      </Fragment>
+    )
+  }
 }
 
 export default Index
--- src/pages/bdoc/view/index.js
+++ src/pages/bdoc/view/index.js
@@ -15,57 +15,55 @@ import './styles.css'
 import 'antd/dist/antd.css'
 
 const tabListNoTitle = [
-    {
-        key: 'Document',
-        tab: 'Document',
-    },
-    {
-        key: 'Details',
-        tab: 'Details',
-    },
-
-];
-
+  {
+    key: 'Document',
+    tab: 'Document',
+  },
+  {
+    key: 'Details',
+    tab: 'Details',
+  },
+]
 
 class Index extends Component {
-    state = {
-        noTitleKey: 'Document',
-    };
+  state = {
+    noTitleKey: 'Document',
+  }
 
-    tableChangeHandler = () => {
+  tableChangeHandler = () => {}
+  onTabChange = (key, type) => {
+    console.log(key, type)
+    this.setState({ [type]: key })
+  }
 
+  render() {
+    const { bdocDetails } = this.props
+    const contentListNoTitle = {
+      Document: <Documents pages={bdocDetails.pages} />,
+      Details: <Details metadata={bdocDetails.metadata} />,
     }
-    onTabChange = (key, type) => {
-        console.log(key, type);
-        this.setState({ [type]: key });
-    };
-
-    render() {
-        const { bdocDetails } = this.props
-        const contentListNoTitle = {
-            Document: <Documents pages={bdocDetails.pages} />,
-            Details: <Details metadata={bdocDetails.metadata} />,
-        };
-        return (
-            <div>
-                <Card
-                    style={{ width: '100%' }}
-                    title="Document View"
-                    tabList={tabListNoTitle}
-                    activeTabKey={this.state.noTitleKey}
-                    onTabChange={key => {
-                        this.onTabChange(key, 'noTitleKey');
-                    }}
-                >
-                    {contentListNoTitle[this.state.noTitleKey]}
-                </Card>
-            </div>
-        )
-    }
+    return (
+      <div>
+        <Card
+          title="Document View"
+          tabList={tabListNoTitle}
+          activeTabKey={this.state.noTitleKey}
+          onTabChange={key => {
+            this.onTabChange(key, 'noTitleKey')
+          }}
+        >
+          {contentListNoTitle[this.state.noTitleKey]}
+        </Card>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => ({
-    bdocDetails: state.bdocReader
+  bdocDetails: state.bdocReader,
 })
 
-export default connect(mapStateToProps, null)(Index)
+export default connect(
+  mapStateToProps,
+  null,
+)(Index)
