d6526e1c2e563a727f978ee7b9b533b47af33884
pranavdaa <pranavmaheshwari4321@gmail.com>
2019-06-11 02:25:12 +0530

    ..

---
+++ src/pages/createcertificate/downloadexcel/index.js
@@ -0,0 +1,23 @@
+// import React from 'react'
+// import ReactExport from 'react-data-export'
+
+// const ExcelFile = ReactExport.ExcelFile
+// const ExcelSheet = ReactExport.ExcelFile.ExcelSheet
+// const ExcelColumn = ReactExport.ExcelFile.ExcelColumn
+
+// class Download extends React.Component {
+//   render() {
+//     return (
+//       <ExcelFile element={<button>Download Data</button>}>
+//         <ExcelSheet data={} name="Employees">
+//           <ExcelColumn label="Name" value="name" />
+//           <ExcelColumn label="Wallet Money" value="amount" />
+//           <ExcelColumn label="Gender" value="sex" />
+//         </ExcelSheet>
+
+//       </ExcelFile>
+//     )
+//   }
+// }
+
+// export default Download
--- src/pages/upload/exl/index.js
+++ src/pages/upload/exl/index.js
@@ -62,7 +62,6 @@ class UploadExcel extends React.Component {
                 <p className="ant-upload-drag-icon">
                   <Icon type="inbox" />
                 </p>
-                <p className="ant-upload-text">Click or drag file to this area to upload</p>
                 <p className="ant-upload-hint">
                   Support for a single or bulk upload. Strictly prohibit from uploading company data
                   or other band files
